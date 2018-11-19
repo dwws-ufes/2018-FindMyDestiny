2a1d7cb1ca0c914a284ff7f4ba20d95a97fe75f9
Guilherme <eeeee.guilherme@gmail.com>
2018-11-18 23:32:58 -0200

    Ignore configuration files and misc

--- misc/eclipse.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start C:\Users\Elogroup\eclipse\java-photon\eclipse\eclipse.exe -nosplash -data "W:"
--- misc/emacs.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start C:\Software\emacs\bin\runemacs.exe --load=C:\Users\Elogroup\.emacs.d\.emacs
